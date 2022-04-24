28ba017c3b26dfd6910031537a1ee1451c0cd3b3
louis70109 <louis70109@gmail.com>
2022-04-24 16:30:46 +0800

    add TAB and ENTER event listener

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="VideoPlayer">
+    <button>Dump VTT</button>
+
     <h2>Editor</h2>
     <div>Current Time: {{ playTime }}</div>
     <div>Subtitle start_time: {{ editorObject['start_time'] }}</div>
     <div>Subtitle end_time: {{ editorObject['end_time'] }}</div>
-
+    <button @click="inputHtmlValueToSubtitleText">Sync</button>
     <br />
-
-    <span v-for="(editor, index) in editorEdit" :key="index">
-      <input :id="index" type="text" :value="editor" />
-    </span>
+    <div>
+      <span
+        class="editor-list"
+        v-for="(editor, index) in editorEdit"
+        :key="index"
+        :id="'editor_span-' + index"
+      >
+        <input :id="'editor-' + index" type="text" :value="editor" />
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
 import { onMounted, onUnmounted, ref } from 'vue';
-
+import { inputHtmlValueToSubtitleText } from '../utils/editor';
 export default {
   name: 'Editor',
   setup() {
@@ -26,6 +34,42 @@ export default {
       editorEdit = ref([]),
       editorRestore = ref([]);
     // description = ref(editorObject.value.description);
+
+    function tabKeyPressMergeColumn(el, editorIdIndex) {
+      let editor = document.getElementById(`editor-${editorIdIndex}`),
+        editorNext = null;
+
+      // Find next element HTML
+      for (
+        let idx = editorIdIndex + 1;
+        idx < editorEdit.value.length;
+        idx++
+      ) {
+        editorNext = document.getElementById(`editor-${(editorIdIndex += 1)}`);
+        if (editorNext !== null) break;
+      }
+
+      // need to judge last element
+      if (el.which == 9) {
+        // merge to next editor input column
+        editorNext.value = editor.value + editorNext.value;
+        console.log(editorNext.value);
+        editor.parentNode.remove();
+        console.log(`#editor-${editorIdIndex} upup`);
+      }
+    }
+    function enterKeyPress(el, editorIdIndex) {
+      let editor = document.getElementById(`editor-${editorIdIndex}`);
+      let parent = editor.parentNode;
+      const wrapper = document.createElement('div');
+
+      // set the wrapper as child (instead of the element)
+      parent.replaceChild(wrapper, editor);
+      // set element as child of wrapper
+      wrapper.appendChild(editor);
+
+      console.log(editorObject);
+    }
     onMounted(async () => {
       editorEdit.value = editorObject.description.split(', ');
       editorRestore.value = editorEdit.value;
@@ -33,6 +77,28 @@ export default {
       setInterval(() => {
         playTime.value = Number(localStorage.getItem('lastVideoPlayTime'));
       }, 2000);
+
+      // add event listener on editor input column
+      let editorCallback = () => {
+        const totalEditorTextLength = editorEdit.value.length;
+        for (let idx = 0; idx < totalEditorTextLength; idx++) {
+          console.log('------------------');
+          // Tab click event
+          document
+            .getElementById(`editor-${idx}`)
+            .addEventListener('keydown', (el) => {
+              tabKeyPressMergeColumn(el, idx);
+            });
+          //  Enter click event
+          document
+            .getElementById(`editor-${idx}`)
+            .addEventListener('keydown', (el) => {
+              enterKeyPress(el, idx);
+            });
+          clearInterval(editorEventSetter);
+        }
+      };
+      const editorEventSetter = setInterval(editorCallback, 2000);
     });
     onUnmounted(() => {});
 
@@ -43,6 +109,7 @@ export default {
       editorEdit,
       editorRestore,
       playTime,
+      inputHtmlValueToSubtitleText,
     };
   },
 };
